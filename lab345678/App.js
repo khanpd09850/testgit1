e0d08790bb503698c688a7a955aaf1582223555b
khanpd09850 <khanpd09850@fpt.edu.vn>
2025-01-15 10:56:21 +0700

    lab3

--- lab345678/App.js
+++ lab345678/App.js
@@ -1,33 +1,8 @@
 import react from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-export const ViewComponet = () => {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={styles.baseText}>
-          Em vao doi ban
-          <Text style={styles.textPage}>Van ban 2</Text>
-        </Text>
-      </View>
-    </ScrollView>
-  );
-};
-export default ViewComponet;
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "blue",
-    height: 200,
-    width: 200,
-    alignSelf: "center",
-    marginTop: 20,
-  },
-  baseText: {
-    fontSize: 15,
-    color: "red",
-  },
-  textPage: {
-    fontWeight: "bold",
-    color: "#111",
-  },
-});
+import Lab3 from "./lab3";
+const App = () => {
+  return <Lab3></Lab3>;
+};
+
+export default App;
